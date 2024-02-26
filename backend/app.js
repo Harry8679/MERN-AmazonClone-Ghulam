e20eecda2206914606ba88743623de8af46af9bb
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-27 00:20:53 +0100

    First example for get all products

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 dotenv.config({ path: "backend/config/config.env" });
 
+// Import all routes
+import productRoutes from "./routes/product.route.js";
+
+app.use("/api/v1", productRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server is running on the port ${process.env.PORT} in ${process.env.NODE_ENV} mode`
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/product.controller";
+import { getProducts } from "../controllers/product.controller.js";
+// const router = express.Router();
 const router = express.Router();
 
-router.route("/products").get(getProducts);
+// router.route("/products").get(getProducts);
+router.route('/products').get(getProducts);
+
+export default router;
