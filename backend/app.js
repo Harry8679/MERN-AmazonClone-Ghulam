d11ca08d4cc4e463e2472b901d2a2885c0521845
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-28 10:26:41 +0100

    Create a new Product

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ dotenv.config({ path: "backend/config/config.env" });
 // Connection Database
 connectDatabase();
 
+app.use(express.json());
+
 // Import all routes
 import productRoutes from "./routes/product.route.js";
 import { connectDatabase } from "./config/dbConnect.js";
--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,17 @@
+import Product from '../models/product.model.js';
+
+// Get All Products => /api/v1/products
 export const getProducts = async (req, res) => {
     res.status(200).json({
         message: 'All products'
+    });
+}
+
+// Get All Products => /api/v1/products
+export const newProducts = async (req, res) => {
+    const product = await Product.create(req.body);
+    
+    res.status(200).json({
+        product
     });
 }
