e126860ec682cbf6cf83c78e93a0b0126716e419
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-28 11:41:07 +0100

    Delete a Product

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -12,7 +12,7 @@ export const getProducts = async (req, res) => {
 export const newProducts = async (req, res) => {
   const product = await Product.create(req.body);
 
-  res.status(200).json({
+  res.status(201).json({
     product,
   });
 };
@@ -30,4 +30,38 @@ export const getProductDetails = async (req, res) => {
   res.status(200).json({
     product,
   });
+};
+
+// Update product details => /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if (!product) {
+        return res.status(404).json({
+            error: 'Product not found.'
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { new: true });
+
+    res.status(200).json({
+        product
+    });
+};
+
+// Delete product => /api/v1/products/:id
+export const deleteProduct = async (req, res) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if (!product) {
+        return res.status(404).json({
+            message: 'Product Deleted'
+        });
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        message: 'Product Deleted'
+    });
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { getProductDetails, getProducts, newProducts } from "../controllers/product.controller.js";
+import {
+  deleteProduct,
+  getProductDetails,
+  getProducts,
+  newProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
 // const router = express.Router();
 const router = express.Router();
 
-router.route('/products').get(getProducts);
-router.route('/admin/products').post(newProducts);
-router.route('/products/:id').get(getProductDetails);
+router.route("/products").get(getProducts);
+router.route("/admin/products").post(newProducts);
+router.route("/products/:id").get(getProductDetails);
+router.route("/admin/products/:id").patch(updateProduct);
+router.route("/admin/products/:id").delete(deleteProduct);
 
 export default router;
