a6ccb6aca98b8efeb1138cac401721338bc056e0
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-28 10:29:47 +0100

    Display all products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,17 +1,18 @@
-import Product from '../models/product.model.js';
+import Product from "../models/product.model.js";
 
 // Get All Products => /api/v1/products
 export const getProducts = async (req, res) => {
-    res.status(200).json({
-        message: 'All products'
-    });
-}
+  const products = await Product.find();
+  res.status(200).json({
+    products,
+  });
+};
 
 // Get All Products => /api/v1/products
 export const newProducts = async (req, res) => {
-    const product = await Product.create(req.body);
-    
-    res.status(200).json({
-        product
-    });
-}
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
+  });
+};
