88c6f2736b18babd5cd2d88cca5b445b3b569f87
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-28 15:52:49 +0100

    Validation & Mongoose ID Error

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -1,20 +1,40 @@
+import ErrorHandler from "../utils/errorHandler.util.js";
+
 export default (err, req, res, next) => {
-    let error = {
-        statusCode: err?.statusCode || 500,
-        message: err?.message || 'Internal Server Error',
-    };
+  let error = {
+    statusCode: err?.statusCode || 500,
+    message: err?.message || "Internal Server Error",
+  };
 
-    if (process.env.NODE_ENV === "DEVELOPMENT") {
-        res.status(error.statusCode).json({
-            message: error.message,
-            error: err,
-            stack: err?.stack
-        });
-    }
+  // Handle Invalid Mongoose ID Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new ErrorHandler(message, 404);
+  }
 
-    if (process.env.NODE_ENV === "PRODUCTION") {
-        res.status(error.statusCode).json({
-            message: error.message
-        });
-    }
+  // Handle Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Handle Validation Error
+  //   if (err.name === 'ValidatorError') {
+  //     const message = Object.values(err.errors).map(value => value.message);
+  //     error = new ErrorHandler(message, 400);
+  //   }
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
 };
