3ee8f01254aff5e454033684e4ffed9b98aa5ae4
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-28 12:21:34 +0100

    Creating Errors Middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// Import all routes
+import productRoutes from "./routes/product.route.js";
+import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
 const app = express();
 
 dotenv.config({ path: "backend/config/config.env" });
@@ -10,11 +15,10 @@ connectDatabase();
 
 app.use(express.json());
 
-// Import all routes
-import productRoutes from "./routes/product.route.js";
-import { connectDatabase } from "./config/dbConnect.js";
-
 app.use("/api/v1", productRoutes);
+
+// Using error middleware
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(
--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import ErrorHandler from "../utils/errorHandler.util.js";
 
 // Get All Products => /api/v1/products
 export const getProducts = async (req, res) => {
@@ -18,13 +19,14 @@ export const newProducts = async (req, res) => {
 };
 
 // Get Single product details => /api/v1/products:id
-export const getProductDetails = async (req, res) => {
+export const getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
 
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found.",
-    });
+    return next(new ErrorHandler("Product not found.", 404));
+    // return res.status(404).json({
+    //   error: "Product not found.",
+    // });
   }
 
   res.status(200).json({
@@ -34,34 +36,36 @@ export const getProductDetails = async (req, res) => {
 
 // Update product details => /api/v1/products/:id
 export const updateProduct = async (req, res) => {
-    let product = await Product.findById(req?.params?.id);
+  let product = await Product.findById(req?.params?.id);
 
-    if (!product) {
-        return res.status(404).json({
-            error: 'Product not found.'
-        });
-    }
-
-    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { new: true });
-
-    res.status(200).json({
-        product
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found.",
     });
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({
+    product,
+  });
 };
 
 // Delete product => /api/v1/products/:id
 export const deleteProduct = async (req, res) => {
-    const product = await Product.findById(req?.params?.id);
+  const product = await Product.findById(req?.params?.id);
 
-    if (!product) {
-        return res.status(404).json({
-            message: 'Product Deleted'
-        });
-    }
-
-    await product.deleteOne();
-
-    res.status(200).json({
-        message: 'Product Deleted'
+  if (!product) {
+    return res.status(404).json({
+      message: "Product Deleted",
     });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    message: "Product Deleted",
+  });
 };
